22f0240edf5e4fa2bc5b58b052a80ec68d6f0834
Cyrus Sahba <27029457+csahba@users.noreply.github.com>
2022-07-12 21:49:22 +0100

    Added rounds and overall winner function

--- manual_rps.py
+++ manual_rps.py
@@ -12,20 +12,38 @@ def get_user_choice():
             return userchoice
 
 def get_winner(computer_choice, user_choice):
+    
     print(f'Computer chose {computer_choice}, User chose {user_choice}')
     if computer_choice == user_choice:
         print('Draw!')
-        return
+        return 0
     if (computer_choice =='rock' and user_choice =='paper') or (computer_choice =='paper' and user_choice =='scissors') or (computer_choice =='scissors' and user_choice =='rock'):
         print('User wins!')
+        return 1
     else:
         print('Computer wins!')
+        return 2
+
+def get_overall_winner(score):
+    if score[1]==score[2]:
+        print("Draw overall!")
+        return
+    if score[1]>score[2]:
+        print("User wins overall!")
+        return
+    print("Computer wins overall!")
+
+def play(total_rounds):
+    rounds = 1
+    score = [0,0,0] # count of draws, user wins, computer wins
+    while rounds<=total_rounds:
+        print(f"Start round {rounds}")
+        computer_choice = get_computer_choice()
+        user_choice = get_user_choice()
+        score[get_winner(computer_choice, user_choice)]+=1
+        rounds+=1
+
+    get_overall_winner(score)
     return
 
-def play():
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
-    get_winner(computer_choice, user_choice)
-    return
-
-play()
+play(5)
